2287c76797e76efcc391f91ac727f9bccc7fc609
astrowq <brandon@qwang.org>
2021-10-03 22:10:36 +0200

    fight warnings

--- dspace-api/src/main/java/org/dspace/eperson/EPersonCLITool.java
+++ dspace-api/src/main/java/org/dspace/eperson/EPersonCLITool.java
@@ -451,7 +451,7 @@ public class EPersonCLITool {
         // wild or regex match user/netid
         // select details (pseudo-format string)
         try {
-            for (EPerson person : ePersonService.findAll(context, EPerson.EMAIL)) {
+            for (EPerson person : ePersonService.findAll(context, EPerson.EMAIL, 20, 0)) {
                 System.out.printf("%s\t%s/%s\t%s, %s\n",
                                   person.getID().toString(),
                                   person.getEmail(),
